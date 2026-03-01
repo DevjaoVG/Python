6d9c3ebd82d57287152755170b6cf2938afedd74
DevjaoVG <joaovitorcontato1247@gmail.com>
2026-02-28 23:27:36 -0300

    feat: exemplo sintaxe básica

---
+++ fundamentos/01_Sintaxe_Basica.py
@@ -0,0 +1,21 @@
+""""
+Exemplo simples de script em Python
+Demonstra a sintaxe básica de Python, incluindo comentários, variáveis e impressão de saída.
+Autor: João
+""""
+
+# Variáveis
+nome
+mensagem
+
+# Entrada de dados
+nome = input("Digite seu nome: ")
+
+# Processamento usando condicional
+if nome:
+    mensagem = f"Olá, {nome}! Bem-vindo ao Python!"
+else:
+    mensagem = "Olá! Bem-vindo ao Python!"
+
+# Saída de dados
+print(mensagem)
