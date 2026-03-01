8714eb7dee097bcc4eb9d26d97ac78d04ca919cc
DevjaoVG <joaovitorcontato1247@gmail.com>
2026-02-28 23:43:19 -0300

    feat: Estruturas Condicionais

---
+++ fundamentos/04_Estruturas_Condicionais.py
@@ -0,0 +1,19 @@
+"""
+Demonstrando o uso de estruturas condicionais em Python, como if, elif e else.
+"""
+
+# Exemplo de estrutura condicional IF e ELSE
+idade = 18
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
+
+# Exemplo de estrutura condicional IF, ELIF e ELSE
+nota = 85
+if nota >= 90:
+    print("Excelente!")
+elif nota >= 70:
+    print("Bom trabalho!")
+else:
+    print("Precisa melhorar.")
