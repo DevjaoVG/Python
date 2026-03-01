9eaff5097d388008fd0a8df8126e05df3ad40861
DevjaoVG <joaovitorcontato1247@gmail.com>
2026-03-01 00:15:12 -0300

    feat: Estutura de Repetição

---
+++ fundamentos/05_Estruturas_de_Repetição.py
@@ -0,0 +1,13 @@
+"""
+Demonstração de estruturas condicionais For e While.
+"""
+
+# Estrutura de repetição For
+for i in range(5):
+    print(f"Contagem: {i}")
+
+# Estrutura de repetição While
+contador = 0
+while contador < 5:
+    print(f"Contagem: {contador}")
+    contador += 1
