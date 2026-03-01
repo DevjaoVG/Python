6ef79e3eb475bd2043e0ba77af02748370e2834a
DevjaoVG <joaovitorcontato1247@gmail.com>
2026-03-01 00:18:03 -0300

    feat: funções

---
+++ fundamentos/06_Funcoes.py
@@ -0,0 +1,16 @@
+"""
+Demosntração de funções.
+"""
+
+# Saudação
+def saudacao(nome):
+    print(f"Olá, {nome}! Bem-vindo(a) ao curso de Python.")
+
+saudacao("João")
+
+# Soma
+def soma(a, b):
+    return a + b
+
+resultado = soma(5, 3)
+print(f"A soma de 5 e 3 é: {resultado}")
